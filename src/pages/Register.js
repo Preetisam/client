559cc0a98c3448afd07f71d9194b25ef392ede06
Preetisam <preetikamilla@gmail.com>
2023-06-17 22:19:09 +0530

    update

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,57 +1,85 @@
-import React from 'react';
-import { Form, Input, message } from 'antd';
-import axios from 'axios';
-import { Link, useNavigate} from 'react-router-dom';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import '../styles/RegisterStyles.css';
 
 function Register() {
+  const [password, setPassword] = useState("");
+  const [userName, setUserName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
   const navigate = useNavigate();
 
-  // form handler
-  const onFinishHandler = async (values) => {
-    try {
-      const res = await axios.post('/api/v1/user/register', values); // add the backend API endpoint URL
-      if (res.data.success) {
-        message.success('Register Successfully! ');
-        navigate('/login');
-      } else {
-        message.error(res.data.message);
-      }
-    } catch (error) {
-      console.log(error);
-      message.error('Something Went Wrong');
-    }
+
+  const signupFn = async (e) => {
+    e.preventDefault();
+    const body = {
+      name: userName,
+      email: userEmail,
+      password: password,
+    };
+    let data = await axios.post("http://localhost:8080/crm/api/v1/auth/signup", body);
+    console.log("Data is " + JSON.stringify(data));
   };
 
+
   return (
-    <>
-      <div className='form-container'>
-        <Form layout='vertical' onFinish={onFinishHandler} className='register-form'>
-          <h3 className='text-center'>Register Form</h3>
-
-          <Form.Item label='Name' name='name'>
-            <Input type='text' required/>
-          </Form.Item>
-
-          <Form.Item label='Email' name='email'>
-            <Input type='email' required/>
-          </Form.Item>
-
-          <Form.Item label='Password' name='password'>
-            <Input type='password' required/>
-          </Form.Item>
-
-          <Link to='/login' className='m-2'>
-            Already user login here
-          </Link>
-
-          <button className='btn btn-primary' type='submit'>
-            Register
-          </button>
-        </Form>
+    <div>
+      <div className="bg-info d-flex justify-content-center align-items-center vh-100">
+        <div className="card card-signin m-5 p-5 shadow-lg rounded-4">
+          <div className="row m-2">
+            <div>
+              <h4 className="text-center">
+            Sign Up
+              </h4>
+              <form onSubmit = {signupFn}>
+                  
+                    <div className="input-group m-1">
+                      <input
+                        type="userName"
+                        className="form-control"
+                        placeholder="User Name"
+                        id="userName"
+                        value={userName}
+                        onChange={(e) => setUserName(e.target.value)}
+                        required
+                      />
+                    </div>
+                    <div className="input-group m-1">
+                      <input
+                        type="email"
+                        className="form-control"
+                        placeholder="Email"
+                        id="email"
+                        value={userEmail}
+                        onChange={(e) => setUserEmail(e.target.value)}
+                        required
+                      />
+                    </div>
+                <div>
+                  <input
+                    type="password"
+                    className="form-control m-1"
+                    placeholder="Password"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="input-group my-2 ms-1">
+                  <input
+                    type="submit"
+                    className="form-control btn btn-primary"
+                    value='Sign Up'
+                  />
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
-export default Register;
+export default Register;
