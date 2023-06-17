461c40ed8835f7dc99433f4be0eeff75ebbdf8f1
Preetisam <preetikamilla@gmail.com>
2023-06-17 18:56:34 +0530

    doctor

---
+++ src/pages/HomePages.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Homepages() {
+  return (
+    <div>
+      <h1>Home pages</h1>
+    </div>
+  )
+}
+
+export default Homepages
---
+++ src/pages/Login.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import {Form,Input} from 'antd'
+import {Link} from 'react-router-dom'
+import '../styles/LoginStyles.css'
+
+function Login() {
+
+     //form handler
+  const onfinishHandler=(values)=>{
+    console.log(values)
+  }
+  return (
+    <>
+ <div className='form-container'>
+  <Form layout='vertical' onFinish={onfinishHandler} className='login-form'>
+   <h3 className='text-center'>Login Form</h3>
+
+  <Form.Item label="Name" name='name'>
+  <Input type="text" required/>
+  </Form.Item>
+  <Form.Item label="Email" name='email'>
+  <Input type="email" required/>
+  </Form.Item>
+  <Form.Item label="Password" name='password'>
+  <Input type="password" required/>
+  </Form.Item>
+  <Link to='/register' className='m-2'>Not a  User? please login here</Link>
+  <button className='btn btn-primary' type='submit'>Login</button>
+  </Form>
+ </div>
+
+    </>
+  )
+}
+
+export default Login
